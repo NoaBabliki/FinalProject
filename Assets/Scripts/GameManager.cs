754bc3abe2c6e5b6e63859a625e423b5bbc939be
ShirSagy <shir.sagy.41@gmail.com>
2022-01-31 02:38:48 +0200

    basically everything is working except buttons.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,13 +6,17 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] private int overallSuccess = 3;
     [SerializeField] private Light largeSpot;
-   
+    [SerializeField] private Material successSkybox;
     [SerializeField] private AudioSource winAudio;
     static private int successCount = 0;
+    private Light _lightIntencity;
+    private float _maxIntensity;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _lightIntencity = largeSpot.GetComponent<Light>();
+        _maxIntensity = _lightIntencity.intensity;
+        _lightIntencity.intensity = 0f;
     }
 
     // Update is called once per frame
@@ -20,17 +24,16 @@ public class GameManager : MonoBehaviour
     {
         // check win
         if (successCount == overallSuccess){
+            RenderSettings.skybox = successSkybox;
             Gaslamp.gaslightNumberOn = 0;
-            Light lightIntencity = largeSpot.GetComponent<Light>();
-            if (lightIntencity.intensity < 1.0f){
-                lightIntencity.intensity += 0.5f * Time.deltaTime;  // Time.deltaTime = time[s] from last frame
+            
+            if (_lightIntencity.intensity <= _maxIntensity){
+                _lightIntencity.intensity += 0.1f * Time.deltaTime;  // Time.deltaTime = time[s] from last frame
             }
             Debug.Log("ITS A WIN");
             MainAudio.StopAll();
             winAudio.GetComponent<AudioSource>().Play();
         }
-
-        
     }
 
     public static void AddSuccess() {
